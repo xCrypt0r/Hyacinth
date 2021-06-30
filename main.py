0a380eb997e0f4284c79c0f4c467410fc585e311
SoBusted <fireintheholl@naver.com>
2021-07-01 07:10:57 +0900

    Add gui

--- main.py
+++ main.py
@@ -1,11 +1,83 @@
+import sys
 import json
+from PySide2.QtGui import QIcon
+from PySide2.QtWidgets import *
 from sweeper import DCSweeper
 
-if __name__ == '__main__':
-    with open('const.json', encoding='utf8') as const_file:
-        const = json.load(const_file)
-        target_galleries = const['target_galleries']
-        galleries = const['galleries']
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
 
-    for target_gallery in target_galleries:
-        DCSweeper(target_gallery, galleries[target_gallery]).start_sweeping()
+        self.sweepers = []
+
+        with open('const.json', encoding='utf8') as const_file:
+            const = json.load(const_file)
+            self.galleries = const['galleries']
+
+        self.load_ui()
+
+    def load_ui(self):
+        self.setWindowTitle('DCSweeper')
+        self.setGeometry(100, 100, 500, 300)
+        self.setWindowIcon(QIcon('assets/icon.png'))
+
+        qr = self.frameGeometry()
+        cp = QApplication.primaryScreen().availableGeometry().center()
+
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+        self.cmb_galleries = QComboBox(self)
+        galleries = list(sorted(self.galleries.values()))
+
+        self.cmb_galleries.addItems(galleries)
+        self.cmb_galleries.setGeometry(20, 20, 140, 20)
+        self.cmb_galleries.setCurrentIndex(galleries.index('국내야구'))
+
+        self.btn_add = QPushButton('추가', self)
+
+        self.btn_add.setGeometry(165, 20, 40, 20)
+        self.btn_add.clicked.connect(self.add_target)
+
+        self.tbl_targets = QTableWidget(0, 2, self)
+
+        self.tbl_targets.setHorizontalHeaderLabels(['갤러리', '화력'])
+        self.tbl_targets.setGeometry(20, 60, 460, 220)
+
+        header = self.tbl_targets.horizontalHeader()
+
+        header.setSectionResizeMode(0, QHeaderView.Stretch)
+        header.setSectionResizeMode(1, QHeaderView.ResizeToContents)
+
+    def add_target(self):
+        gallery_title = self.cmb_galleries.currentText()
+        gallery_id = self.get_gallery_id(gallery_title)
+        index = self.tbl_targets.rowCount()
+
+        self.tbl_targets.insertRow(index)
+        self.tbl_targets.setItem(index, 0, QTableWidgetItem(gallery_title))
+        self.tbl_targets.setItem(index, 1, QTableWidgetItem('0'))
+
+        sweeper = DCSweeper(self, index, gallery_id, gallery_title)
+
+        sweeper.start_sweeping()
+        self.sweepers.append(sweeper)
+
+    def get_gallery_id(self, gallery):
+        for id, name in self.galleries.items():
+            if name == gallery:
+                return id
+
+    def update(self, index, count):
+        self.tbl_targets.setItem(index, 1, QTableWidgetItem(str(count)))
+
+    def closeEvent(self, QCloseEvent):
+        for sweeper in self.sweepers:
+            sweeper.stop_sweeping()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Window()
+
+    window.show()
+    app.exec_()
--- sweeper.py
+++ sweeper.py
@@ -2,29 +2,46 @@ import requests
 import threading
 import os.path
 import urllib3
+import http
 from urllib import request
 from pathlib import Path
 from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 
 class DCSweeper:
-    def __init__(self, target_gallery, gallery_title):
+    def __init__(self, gui, index, target_gallery, gallery_title):
+        self._timer = None
+        self.gui = gui
+        self.index = index
         self.list_url = 'https://gall.dcinside.com/board/lists/'
         self.post_url = 'https://gall.dcinside.com/board/view/'
         self.target_gallery = target_gallery
         self.gallery_title = gallery_title
-        self.headers = { 'User-Agent': '' }
+        self.ua = UserAgent()
         self.post_sweeped = []
+        self.image_count = 0
 
     def start_sweeping(self):
         self.sweep_images_from_post(self.get_target_post())
-        threading.Timer(2, self.start_sweeping).start()
+
+        self._timer = threading.Timer(2, self.start_sweeping)
+
+        self._timer.start()
+
+    def stop_sweeping(self):
+        self._timer.cancel()
 
     def get_target_post(self):
         req = requests.get(self.list_url,
             params={ 'id': self.target_gallery },
-            headers=self.headers)
+            headers={ 'User-Agent': self.ua.random })
         soup = BeautifulSoup(req.content, 'html.parser')
-        target = soup.find('tbody').find('tr', { 'data-type': 'icon_pic' })
+        tbody = soup.find('tbody')
+
+        if tbody is None:
+            return
+
+        target = tbody.find('tr', { 'data-type': 'icon_pic' })
         target_post = target['data-no']
 
         return target_post
@@ -38,17 +55,27 @@ class DCSweeper:
         try:
             req = requests.get(self.post_url,
                 params={ 'id': self.target_gallery, 'no': target_post },
-                headers=self.headers)
-        except (requests.exceptions.ConnectionError, urllib3.exceptions.MaxRetryError):
+                headers={ 'User-Agent': self.ua.random },
+                timeout=5)
+        except (
+            requests.exceptions.RequestException,
+            urllib3.exceptions.MaxRetryError,
+            http.client.RemoteDisconnected,
+            TimeoutError,
+            OSError
+        ):
             return
 
         soup = BeautifulSoup(req.content, 'html.parser')
         appending_file = soup.find('ul', class_='appending_file')
-        post_title = soup.find('span', class_='title_subject').text
-        post_date = soup.find('span', class_='gall_date').text
 
         if appending_file is None:
             return
+
+        post_title = soup.find('span', class_='title_subject').text
+        post_date = soup.find('span', class_='gall_date').text
+
+
 
         print(f'[{post_date[-8:]}] <{self.gallery_title}> {post_title}')
 
@@ -58,10 +85,13 @@ class DCSweeper:
             attachment_url = attachment.find('a', href=True)['href']
             extension = os.path.splitext(attachment_url)[1]
             opener = request.build_opener()
-
-            opener.addheaders = [('User-agent', ''), ('Referer', req.url)]
+            opener.addheaders = [('User-agent', self.ua.random), ('Referer', req.url)]
 
             Path(f'archive/{self.gallery_title}').mkdir(parents=True, exist_ok=True)
             request.install_opener(opener)
             request.urlretrieve(attachment_url,
                 f'archive/{self.gallery_title}/{self.target_gallery}_{target_post}_{i + 1}{extension}')
+
+            self.image_count += 1
+
+        self.gui.update(self.index, self.image_count)
